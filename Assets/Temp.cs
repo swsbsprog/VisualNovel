fd48b0bb3f91b3a3af7f346409c6c9341406b983
chanseon.park <chanseon.park@kongstudios.net>
2022-08-27 16:11:20 +0900

    Visual Novel Toolkit Free, DoTween 에셋 추가

---
+++ Assets/Temp.cs
@@ -0,0 +1,19 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.DOMove()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
